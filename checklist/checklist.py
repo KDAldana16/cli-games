df4fd141a01733fe2821942c3afb1c0ca886fd19
Karla Aldana <karla.aldana@students.dominican.edu>
2021-09-14 10:22:05 -0700

    created helper functions

--- checklist/checklist.py
+++ checklist/checklist.py
@@ -16,6 +16,35 @@ def update(index, item):
 def destroy(index):
     checklist.pop(index)
 
+def list_all_items():
+    index = 0
+    for list_item in checklist:
+        print(str(index) + list_item)
+        index += 1
+
+def mark_completed(index):
+    # insert code here
+
+def select(function_code):
+    # create item
+    if function_code == "C":
+        input_item = user_input("Input item:")
+        create(input_item)
+    # read item
+    elif function_code == "R":
+        item_index = user_input("Index Number?")
+        read(item_index)
+
+    elif function_code == "P":
+        list_all_items()
+
+    else:
+        print("Unknown Option")
+
+def user_input(prompt):
+    user_input = input(prompt)
+    return user_input
+
 def test():
     checklist.append("purple sox")
     checklist.append("red cloak")
@@ -27,10 +56,19 @@ def test():
     checklist.pop(1)
 
     print(read(0))
-    print(read(1))
+
+    list_all_items()
+
+    select("C")
+    list_all_items()
+
+    select("R")
+    list_all_items()
+
+    select("P")
+    list_all_items()
+
+    user_value = user_input("Please Enter a value:")
+    print(user_value)
 
 test()
-
-# checklist = ['Blue', 'Cats']
-# checklist.pop(1)
-# print(checklist)
