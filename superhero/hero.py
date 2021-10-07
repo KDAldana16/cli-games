9c293454c9020eeb664f51ca813d5b9d51d7de45
Karla Aldana <karla.aldana@students.dominican.edu>
2021-10-07 09:11:01 -0700

    added abilities and armor

--- superhero/hero.py
+++ superhero/hero.py
@@ -1,3 +1,7 @@
+import random
+
+random.randint(2, 7)
+
 class Hero:
     def __init__(self, name, starting_health=100):
         '''Instance properties:
@@ -21,3 +25,40 @@ if __name__ == "__main__":
     hero2 = Hero("Dumbledore")
 
     hero1.fight(hero2)
+
+class Ability:
+    def __init__(self, name, max_damage):
+        '''
+        Initalize the values passed into this method as instance variables
+        '''
+        self.name = name
+        self.max_damage = max_damage
+
+    def attack(self):
+        '''Return a value between 0 and the value set by self.max_damage.'''
+        random_value = random.randint(0,self.max_damage)
+        return random_value
+
+    if __name__ == "__main":
+        ability = Ability("Debugging Ability", 20)
+        print(ability.name)
+        print(ability.attack())
+
+class Armor:
+    def __init__(self, name, max_block):
+        '''Instantiate instance properties.
+            name: String
+            max_block: Integer
+        '''
+        # TODO: Create instance variables for the values passed in
+        pass
+
+    def block(self):
+        '''return a random value between 0 and the initialized max_block strength.
+        '''
+        pass
+
+    if __name__ == "__main__":
+        armor = Armos("Debugging Shield", 10)
+        print(armor.name)
+        print(armor.block())
