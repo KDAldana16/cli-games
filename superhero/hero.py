ed539376090b238b03f750c43603344ad4f3b04f
Karla Aldana <karla.aldana@students.dominican.edu>
2021-10-12 21:07:13 -0700

    Added battle arena

--- superhero/hero.py
+++ superhero/hero.py
@@ -4,7 +4,91 @@ import weapon import Weapon
 random.randint(2, 7)
 
 from ability import Ability
+from weapon import Weapon
 from armor import Armor
+from hero import Hero
+from team import Team
+
+class Arena:
+    def __init__(self):
+        '''Instantiate properties
+            team_one: None
+            team_two: None
+        '''
+
+    def create_ability(self):
+        '''Prompt for Ability information.
+            return Ability with values from user Input
+        '''
+
+        name = input("What is the ability name?     ")
+        max_damage = input(
+            "What is the max damage of the ability?     ")
+
+            return Ability(name, max_damage)
+
+    def create_weapon(self):
+        '''Prompt user for Weapon information
+            return Weapon with values from user input.
+        '''
+        pass
+
+    def create_armor(self):
+        '''Prompt user for Armor information
+            return Armor with values from user input.
+        '''
+        pass
+
+    def create_hero(self):
+        '''Prompt user for Hero information
+            return Hero with values from user input.
+        '''
+        hero_name = input("Hero's name: ")
+        hero = Hero(hero_name)
+        add_item = None
+        while add_item != "4":
+            add_item = input("[1] Add ability\n[2] Add weapon\n[3] Add armor\n[4] Done adding items\n\nYour choice: ")
+            if add_item == "1":
+            elif add_item == "2":
+            elif add_item == "3":
+        return hero
+
+    def build_team_one(self):
+        '''Prompt the user to build team_one'''
+        numOfTeamMembers = int(input("How many members would you like in Team One?\n"))
+        for i in range(numOfTeamMembers):
+            hero = self.create_hero()
+            self.team_one.add_hero(hero)
+
+    def build_team_two(self):
+        '''Prompt the user to build team two'''
+        numOfTeamMembers2 = int(input("How many members would you like in Team Two?\n"))
+        for i in range(numOfTeamMembers2):
+            hero = self.create_hero()
+            self.team_two.add_hero(hero)
+
+    def show_stats(self)
+        ''prints team statistics to terminal''
+        print("\n")
+        print(self.team_one.name + " statistics: ")
+        self.team_one.stats()
+        print("\n")
+        print(self.team_two.name + " statistics: ")
+        self.team_two.stats()
+        print("\n")
+
+        team_kills = 0
+        team_deaths = 0
+        for hero in self.team_one.heroes:
+            team_kills += hero.kills
+            team_deaths += hero.deaths
+        if team_deaths == 0:
+            team_deaths = 1
+        print(self.team_one.name + " average K/D was " + str(team_kills/team_deaths))
+
+        for hero in self.team_one.heroes:
+            if hero.deaths == 0:
+                print("survived from " + self.team_one.name + ": " + hero.name)
 
 class Hero:
     def __init__(self, name, starting_health=100):
@@ -78,6 +162,11 @@ class Hero:
     self.kills = 0
 
 if __name__ == "__main__":
+    arena = Arena()
+    arena.build_team_one()
+    arena.build_team_two()
+    arena.team_battle()
+    arena.show_stats()
     ability = Ability("Great Debugging", 50)
     another_ability = Ability("Smarty Pants", 90)
     hero = Hero("Grace Hopper", 200)
